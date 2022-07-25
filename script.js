d774444f630301ce4ab29b462dba8b77af63e0ee
mmdoza002 <mmdoza002@gmail.com>
2022-07-25 15:57:32 -0500

    update

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-var timeDisplayEl = $('time-display')
-
-
+var timeDisplayEl = $('#time-display')
 
 function displayTime(){
 
     var rightNow= moment().format('MMM DD, YYYY [at] hh:mm:ss a');
     timeDisplayEl.text(rightNow);
-
+console.log(rightNow);
 }
+
+displayTime();
 
